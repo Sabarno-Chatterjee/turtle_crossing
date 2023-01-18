987feb56aeaaa7d15fcbb8443f2894627d3d8baa
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-18 20:08:43 +0530

    player.move() functional

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
 
+player = Player()
+screen.onkey(fun=player.move, key="Up")
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
--- player.py
+++ player.py
@@ -1,7 +1,18 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.color("black")
+        self.setheading(90)
+        self.goto(x=0,y=-280)
+
+    def move(self):
+        self.forward(20)
+
